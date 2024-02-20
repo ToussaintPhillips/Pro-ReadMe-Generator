8ef8247e4470e757b9984131b1a28c72f636d326
Toussaint Phillips <toussaintphillips@gmail.com>
2024-02-20 19:26:45 +0000

    Update javscript and JSON package

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Pro-ReadMe-Generator
+# test
+
--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const fs = require("fs");
-const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project:",
+  },
+  // Add more questions for other sections of the README
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("README.md generated successfully!")
+  );
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile("README.md", markdown);
+  });
 }
 
 // function call to initialize program
 init();
+
